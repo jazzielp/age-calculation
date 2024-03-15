1fdbe01dc91d978be94b15779ca2e4178db9a5f0
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2024-03-15 14:11:04 -0700

    Add input component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors:{
+			'line' : '#DCDCDC',
+			'white' : '#ffffff',
+			'black' : '#000000',
+			'purple': '#854DFF',
+			'light-grey': '#F0F0F0',
+			'grey': '#716F6F',
+			'red': '#FF5959'
+		},
 		extend: {},
 	},
 	plugins: [],
